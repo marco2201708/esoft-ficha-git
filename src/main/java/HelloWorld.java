a6feebd923d0b3d694f584225030b6c16d3df641
Marco Gaspar <2201708@my.ipleiria.pt>
2022-03-02 09:25:47 +0000

    adicionar janela HelloWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class HelloWorld extends JFrame {
+    private JPanel panel1;
+    public HelloWorld(){
+        setContentPane(panel1);
+        pack();
+    }
+    public static void main(String[] args) {
+        new HelloWorld().setVisible(true);
+    }
+}
